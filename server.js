740bcbb54c8de4dc27441ecc754e994341043cfd
Hunaid <hunaid.hanif@gmail.com>
2022-05-17 22:23:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Manager API' }) )
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`SERVER START ON PORT ${PORT}`))
+app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`))
