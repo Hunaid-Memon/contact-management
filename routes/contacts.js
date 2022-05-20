a5ab5bca6044da5f898a9c3614d16ba4a2f8add4
Hunaid <hunaid.hanif@gmail.com>
2022-05-21 03:37:49 +0500

    Update contact routes - create add a contact API

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,8 +28,35 @@ router.get('/', auth , async (req, res) => {
 // @route   POST api/contacts
 // @desc    Add a new contact
 // @access  Private
-router.post('/', (req, res) => {
-    res.send('Add a Contact')
+router.post('/', [ auth [
+    check('name', 'Name is required').not().isEmpty()
+] ], async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+
+        res.json(contact)
+
+    } catch(err) {
+        console.error(err.message);
+        res.send(500).json('Server Error')
+    }
+
 });
 
 // @route   PUT api/contacts/:id
