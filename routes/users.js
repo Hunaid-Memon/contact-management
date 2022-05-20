a6de1fc19bf40bb0c3f3ee3dc6a8d90e38d8fa99
Hunaid <hunaid.hanif@gmail.com>
2022-05-21 01:47:40 +0500

    Update users routes - API created for user creation

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,54 @@
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
+
 
 const User = require('../models/User')
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Register a user')
+router.post('/', [
+    check('name', 'Please enter a name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter password with atleast 6 characters').isLength({ min: 6 })
+], async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if(user) {
+            return res.status(400).json({ msg: 'A User with this email already exists.' })
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User created in MongoDB')
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+
 });
 
-module.exports = router;
+module.exports = router; 
