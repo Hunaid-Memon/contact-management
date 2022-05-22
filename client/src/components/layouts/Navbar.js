0832f2feea9a29930bfd9f2c3c3ad1596979db01
Hunaid <hunaid.hanif@gmail.com>
2022-05-23 02:17:58 +0500

    Link in Navbar

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
   return (
     <div className='navbar bg-primary' >
-        <h1>
-            <i className={icon} /> {title}
-        </h1>
+        <Link to='/' >
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+        </Link>
+        <ul>
+            <li>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+            </li>
+        </ul>
     </div>
   )
 }
