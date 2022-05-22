a910337601904219b627ee60977f7fb03f38e8a1
Hunaid <hunaid.hanif@gmail.com>
2022-05-23 01:43:53 +0500

    Create Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/layouts/Navbar';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       Contact Management
     </div>
   );
